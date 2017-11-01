8513703ca914777313d69631d7be5811f72e75ec
WinterSunnyJun <496900910@qq.com>
2017-11-01 20:25:10 +0800

    ajun

--- js/js.js
+++ js/js.js
@@ -1,3 +1,16 @@
 /**
  * Created by DELL on 2017/10/31.
  */
+$(function () {
+//    电路图查询
+    $('.layout-list > li').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        $(this).siblings().find('.layout-list-sub').hide();
+        if( $(this).find('.layout-list-sub').css('display') == 'block'){
+            $(this).find('.layout-list-sub').hide();
+        }else{
+            $(this).find('.layout-list-sub').show();
+        }
+
+    })
+})
